fc6411a02fc7fe7115d0b4d27759e40335b3ba26
eejit <eejitgames@gmail.com>
2024-08-04 15:48:36 +0100

    red ghost debug above pen

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -6,40 +6,47 @@ def init(args)
   [ 6,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  6 ],
   [ 6,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  6 ],
   [ 6,  4,  4, 11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 13, 14, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 15,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1,  3,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 26, 28, 28, 19,  1, 26, 28, 28, 28, 19,  1, 21, 23,  1, 26, 28, 28, 28, 19,  1, 26, 28, 28, 19,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  2, 21, 10, 10, 23,  1, 21, 10, 10, 10, 23,  1, 21, 23,  1, 21, 10, 10, 10, 23,  1, 21, 10, 10, 23,  2, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 22, 30, 30, 20,  1, 22, 30, 30, 30, 20,  1, 22, 20,  1, 22, 30, 30, 30, 20,  1, 22, 30, 30, 20,  1, 24,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  3,  1,  1,  1,  1,  3,  1,  1,  3,  1,  1,  3,  1,  1,  3,  1,  1,  3,  1,  1,  3,  1,  1,  1,  1,  3, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 26, 28, 28, 19,  1, 26, 19,  1, 26, 28, 28, 28, 28, 28, 28, 19,  1, 26, 19,  1, 26, 28, 28, 19,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 22, 30, 30, 20,  1, 21, 23,  1, 22, 30, 30, 29, 34, 30, 30, 20,  1, 21, 23,  1, 22, 30, 30, 20,  1, 24,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  3, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  3,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 17, 18, 18, 18, 18, 19,  1, 21, 35, 28, 28, 19,  0, 21, 23,  0, 26, 28, 28, 36, 23,  1, 26, 18, 18, 18, 18, 25,  4,  4,  6 ],
   [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 34, 30, 30, 20,  0, 22, 20,  0, 22, 30, 30, 29, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
-  [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
+  [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0,  0,  0,  4,  0,  0,  4,  0,  0,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
   [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0, 37, 18, 40, 43, 43, 41, 18, 39,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
   [ 6,  4,  4, 12, 12, 12, 12, 12, 20,  1, 22, 20,  0, 24, 10, 10, 10, 10, 10, 10, 16,  0, 22, 20,  1, 22, 12, 12, 12, 12, 12,  4,  4,  6 ],
-  [ 6,  0,  0,  0,  0,  0,  0,  0,  6,  1,  0,  0,  0, 24, 10, 10, 10, 10, 10, 10, 16,  0,  0,  0,  1,  6,  0,  0,  0,  0,  0,  0,  0,  6 ],
+  [ 6,  0,  0,  0,  0,  0,  0,  0,  6,  3,  0,  0,  4, 24, 10, 10, 10, 10, 10, 10, 16,  4,  0,  0,  3,  6,  0,  0,  0,  0,  0,  0,  0,  6 ],
   [ 6,  4,  4, 18, 18, 18, 18, 18, 19,  1, 26, 19,  0, 24, 10, 10, 10, 10, 10, 10, 16,  0, 26, 19,  1, 26, 18, 18, 18, 18, 18,  4,  4,  6 ],
   [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0, 38, 12, 12, 12, 12, 12, 12, 42,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
-  [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
+  [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  4,  0,  0,  0,  0,  0,  0,  0,  0,  4, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
   [ 6,  4,  4, 10, 10, 10, 10, 10, 16,  1, 21, 23,  0, 26, 28, 28, 28, 28, 28, 28, 19,  0, 21, 23,  1, 24, 10, 10, 10, 10, 10,  4,  4,  6 ],
   [ 6,  4,  4, 11, 12, 12, 12, 12, 20,  1, 22, 20,  0, 22, 30, 30, 29, 34, 30, 30, 20,  0, 22, 20,  1, 22, 12, 12, 12, 12, 15,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  3,  1,  1,  3,  1,  1,  3, 21, 23,  3,  1,  1,  3,  1,  1,  3,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 26, 28, 28, 19,  1, 26, 28, 28, 28, 19,  1, 21, 23,  1, 26, 28, 28, 28, 19,  1, 26, 28, 28, 19,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 22, 30, 29, 23,  1, 22, 30, 30, 30, 20,  1, 22, 20,  1, 22, 30, 30, 30, 20,  1, 21, 34, 30, 20,  1, 24,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  2,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1,  1,  0,  0,  1,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  2, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  2,  1,  1, 21, 23,  3,  1,  1,  3,  1,  1,  1,  0,  0,  1,  1,  1,  3,  1,  1,  3, 21, 23,  1,  1,  2, 24,  4,  4,  6 ],
   [ 6,  4,  4, 27, 28, 19,  1, 21, 23,  1, 26, 19,  1, 26, 28, 28, 28, 28, 28, 28, 19,  1, 26, 19,  1, 21, 23,  1, 26, 28, 33,  4,  4,  6 ],
   [ 6,  4,  4, 31, 30, 20,  1, 22, 20,  1, 21, 23,  1, 22, 30, 30, 29, 34, 30, 30, 20,  1, 21, 23,  1, 22, 20,  1, 22, 30, 32,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  1,  1,  3,  1,  1,  3, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  1, 21, 23,  1,  1,  1,  3,  1,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 26, 28, 28, 28, 28, 36, 35, 28, 28, 19,  1, 21, 23,  1, 26, 28, 28, 36, 35, 28, 28, 28, 28, 19,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 16,  1, 22, 30, 30, 30, 30, 30, 30, 30, 30, 20,  1, 22, 20,  1, 22, 30, 30, 30, 30, 30, 30, 30, 30, 20,  1, 24,  4,  4,  6 ],
-  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
+  [ 6,  4,  4, 16,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  3,  1,  1,  3,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 24,  4,  4,  6 ],
   [ 6,  4,  4, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 25,  4,  4,  6 ],
   [ 6,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  6 ],
   [ 6,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  4,  6 ],
   [ 6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6,  6 ]
   ].reverse
+
+=begin
+  "pixel position in map mx, my:   #{32 + args.state.pacman.mx}, #{31 + args.state.pacman.my}",
+  "grid position grid_x, grid_y:   #{args.state.pacman.grid_x}, #{args.state.pacman.grid_y}",
+  "pacman coors on screen x, y:    #{args.state.pacman[:x] + args.state.offset[args.state.pacman.dir].x}, #{args.state.pacman[:y] + args.state.offset[args.state.pacman.dir].y}",
+  "maze status at grid coors:      #{args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x]}",
+=end
 
   args.state.pacman = {
     lives: 3,
@@ -61,15 +68,16 @@ def init(args)
   }
 
   args.state.red_ghost = {
-    x: 32,
-    y: 32,
-    hx: 28,
-    hy: 35,
+    mx: 35,
+    my: 79,
+    hx: 16, # grid coors, home corner when scatter mode
+    hy: 10,
+    grid_x: 16,
+    grid_y: 22,
+    move_x: 0,
+    move_y: 0,
     dir: 0,
   }
-
-  args.state.tunnel_left = :off
-  args.state.tunnel_right = :off
 
   # to position sprite correctly
   args.state.offset[0] = { x: 0, y: 1 }
@@ -87,29 +95,24 @@ def tick(args)
   draw_dots args
   draw_score args
   draw_lives args
-  # draw_tunnel args
   draw_red_ghost args
   draw_pacman args
-
+  render_debug args
   return if game_has_lost_focus?
-
   player_input args
-
   return if args.state.pacman.dir == 0
   move_red_ghost args
-  # render_debug args
-end
-
-def draw_red_ghost(args)
 end
 
 def move_red_ghost(args)
+  # point_to_grid args, (32 + args.state.pacman.mx), (31 + args.state.pacman.my)
+  # point_to_grid_red_ghost args, 0, 0 # (0 - args.state.red_ghost.mx), (0 - args.state.red_ghost.my)
 end
 
 def draw_dots(args)
   args.state.maze.each_with_index do |row, row_index|
     row.each_with_index do |value, col_index|
-      if value == 1
+      if value == 1 || value == 3
         args.lowrez.primitives << {
           x: (col_index * 4 - args.state.pacman.mx) + 1,
           y: (row_index * 4 - args.state.pacman.my) + 2,
@@ -207,6 +210,29 @@ def grid_highlight(args)
   }
 end
 
+def grid_highlight_red_ghost(args)
+  grid_size = 4 # each grid cell is 4x4 pixels
+
+  grid_x = args.state.red_ghost.grid_x
+  grid_y = args.state.red_ghost.grid_y
+
+  # Calculate the screen coordinates for the highlighted grid cell
+  x = grid_x * grid_size
+  y = grid_y * grid_size
+
+  args.lowrez.primitives << {
+    x: (0 - args.state.pacman.mx) + x,
+    y: (0 - args.state.pacman.my) + y,
+    w: grid_size,
+    h: grid_size,
+    r: 0,
+    g: 255,
+    b: 0,
+    a: 128,
+    path: :pixel
+  }
+end
+
 def point_to_grid(args, point_x, point_y)
   # determine which grid section a point is in
   size_x = 4 # 136 / 34
@@ -214,6 +240,16 @@ def point_to_grid(args, point_x, point_y)
 
   args.state.pacman.grid_x = (point_x / size_x).floor
   args.state.pacman.grid_y = (point_y / size_y).floor
+end
+
+
+def point_to_grid_red_ghost(args, point_x, point_y)
+  # determine which grid section a point is in
+  size_x = 4 # 136 / 34
+  size_y = 4 # 148 / 37
+
+  args.state.red_ghost.grid_x = (point_x / size_x).floor
+  args.state.red_ghost.grid_y = (point_y / size_y).floor
 end
 
 # 1 up, 2 right, 3 down, 4 left (0 for stationary until a key is pressed)
@@ -227,28 +263,28 @@ def player_input(args)
   point_y = 31 + args.state.pacman.my
 
   if args.inputs.up
-    if (((args.state.pacman.mx - 1) % 4) == 0) && (args.state.maze[(point_y/ 4).floor + 1][(point_x / 4).floor] < 3)
+    if (((args.state.pacman.mx - 1) % 4) == 0) && (args.state.maze[(point_y/ 4).floor + 1][(point_x / 4).floor] < 2)
       args.state.pacman[:dir] = 1
       move_y = 1
       move_x = 0
       args.state.pacman.anim = :yes
     end
   elsif args.inputs.right
-    if (((args.state.pacman.my + 1) % 4) == 0) && (args.state.maze[(point_y / 4).floor][(point_x / 4).floor + 1] < 3)
+    if (((args.state.pacman.my + 1) % 4) == 0) && (args.state.maze[(point_y / 4).floor][(point_x / 4).floor + 1] < 2)
       args.state.pacman[:dir] = 2
       move_x = 1
       move_y = 0
       args.state.pacman.anim = :yes
     end
   elsif args.inputs.down
-    if (((args.state.pacman.mx - 1) % 4) == 0) && (args.state.maze[(point_y/ 4).floor - 1][(point_x / 4).floor] < 3)
+    if (((args.state.pacman.mx - 1) % 4) == 0) && (args.state.maze[(point_y/ 4).floor - 1][(point_x / 4).floor] < 2)
       args.state.pacman[:dir] = 3
       move_y = -1
       move_x = 0
       args.state.pacman.anim = :yes
     end
   elsif args.inputs.left
-    if (((args.state.pacman.my + 1) % 4) == 0) && (args.state.maze[(point_y / 4).floor][(point_x / 4).floor - 1] < 3)
+    if (((args.state.pacman.my + 1) % 4) == 0) && (args.state.maze[(point_y / 4).floor][(point_x / 4).floor - 1] < 2)
       args.state.pacman[:dir] = 4
       move_x = -1
       move_y = 0
@@ -268,13 +304,13 @@ def player_input(args)
       temp = point_to_grid args, (32 + args.state.pacman.mx + move_x - 1), (31 + args.state.pacman.my + move_y)
   end
 
-  if args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] > 2
+  if args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] > 4
     args.state.pacman.anim = :no
     move_x = 0
     move_y = 0
   end
 
-  if args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] == 1
+  if args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] == 1 || args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] == 3
     args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x] = 0
     args.state.pacman.score += 10
   end
@@ -288,8 +324,27 @@ def player_input(args)
   args.state.pacman.move_y = move_y
   args.state.pacman.mx += move_x
   args.state.pacman.my += move_y
+  args.state.red_ghost.mx += move_x
+  args.state.red_ghost.my += move_y
+
+  args.state.map_origin_x = 32 + args.state.pacman.mx
+  args.state.map_origin_y = 31 + args.state.pacman.my
 
   point_to_grid args, (32 + args.state.pacman.mx), (31 + args.state.pacman.my)
+end
+
+def draw_red_ghost(args)
+  sprite_path  = "sprites/red-ghost.png"
+  args.lowrez.primitives << {
+    x: (0 - args.state.pacman.mx + 1) + 67,
+    y: (0 - args.state.pacman.my) + 90,
+    w: 8,
+    h: 8,
+    path: sprite_path,
+    anchor_x: 0.5, # position horizontally at 0.5 of the sprite's width
+    anchor_y: 0.5, # position vertically at 0.5 of the sprite's height
+    angle: 0
+  }
 end
 
 def draw_pacman(args)
@@ -318,44 +373,6 @@ def draw_pacman(args)
     }
   else
     countdown_in_seconds = ((start_animation_on_tick - Kernel.tick_count) / 60).round(1)
-  end
-end
-
-def draw_tunnel(args)
-  return if (31 + args.state.pacman.my) != 78
-
-  # if left toggle off return
-  if (32 + args.state.pacman.mx) == 37
-    args.state.tunnel_left = :off
-    return
-  end
-
-  # if right toggle off return
-  if (32 + args.state.pacman.mx) == 97
-    args.state.tunnel_right = :off
-    return
-  end
-
-  # if hit left on toggle left on
-  args.state.tunnel_left = :on if (32 + args.state.pacman.mx == 36)
-
-  # if hit right on toggle right on
-  args.state.tunnel_right = :on if (32 + args.state.pacman.mx == 98)
-
-  # if left on draw
-  if args.state.tunnel_left == :on
-  args.lowrez.lines << { x: 0, y: 38, x2: 25, y2: 38, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 0, y: 36, x2: 25, y2: 36, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 0, y: 26, x2: 25, y2: 26, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 0, y: 24, x2: 25, y2: 24, r: 33, g: 33, b: 255 }
-  end
-
-  # if right on draw
-  if args.state.tunnel_right == :on
-  args.lowrez.lines << { x: 39, y: 38, x2: 63, y2: 38, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 39, y: 36, x2: 63, y2: 36, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 39, y: 26, x2: 63, y2: 26, r: 33, g: 33, b: 255 }
-  args.lowrez.lines << { x: 39, y: 24, x2: 63, y2: 24, r: 33, g: 33, b: 255 }
   end
 end
 
@@ -389,6 +406,20 @@ end
 
 def render_debug(args)
   grid_highlight args
+  grid_highlight_red_ghost args
+  # this is the point to determine which part of the grid red ghost is in
+  args.lowrez.primitives << {
+    x: (0 - args.state.pacman.mx) + 67,
+    y: (0 - args.state.pacman.my) + 90,
+    w: 1,
+    h: 1,
+    r: 200,
+    g: 200,
+    b: 200,
+    a: 200,
+    path: :pixel
+  }
+
   # this is the point to determine which part of the grid pacman is in
   args.lowrez.primitives << {
     x: args.state.pacman[:x],
@@ -439,13 +470,17 @@ def render_debug(args)
   args.state.watch_list = [
     "Kernel.tick_count is:           #{Kernel.tick_count}",
     "args.lowrez.mouse_position is:  #{args.lowrez.mouse_position.x}, #{args.lowrez.mouse_position.y}",
-    "args.lowrez.mouse_down tick:    #{args.state.last_click || "never"}",
-    "args.lowrez.mouse_up tick:      #{args.state.last_up || "false"}",
+    # "args.lowrez.mouse_down tick:    #{args.state.last_click || "never"}",
+    # "args.lowrez.mouse_up tick:      #{args.state.last_up || "false"}",
     "pixel position in map mx, my:   #{32 + args.state.pacman.mx}, #{31 + args.state.pacman.my}",
     "grid position grid_x, grid_y:   #{args.state.pacman.grid_x}, #{args.state.pacman.grid_y}",
     "pacman coors on screen x, y:    #{args.state.pacman[:x] + args.state.offset[args.state.pacman.dir].x}, #{args.state.pacman[:y] + args.state.offset[args.state.pacman.dir].y}",
-    "maze status at grid coors:      #{args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x]}",
-    "score:                          #{args.state.pacman.score}"
+    # "maze status at grid coors:      #{args.state.maze[args.state.pacman.grid_y][args.state.pacman.grid_x]}",
+    "red ghost map pos mx, my:       #{32 + args.state.red_ghost.mx}, #{31 + args.state.red_ghost.my}",
+    "red ghost grid grid_x, grid_y:  #{args.state.red_ghost.grid_x}, #{args.state.red_ghost.grid_y}",
+    "red ghost screen coors x, y:    #{args.state.red_ghost[:x]}, #{args.state.red_ghost[:y]}",
+    "args.state.map_origin_x:        #{args.state.map_origin_x}",
+    "args.state.map_origin_y:        #{args.state.map_origin_y}"
   ]
 
   args.outputs.debug << args.state
